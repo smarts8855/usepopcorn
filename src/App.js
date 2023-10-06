eeacf19f22054a17d9e837c4abf9de112abc641b
smarts8855 <85696168+smarts8855@users.noreply.github.com>
2023-10-06 13:05:49 +0100

    escape key

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const average = (arr) =>
 const KEY = "9e8a0017";
 
 export default function App() {
-  const [query, setQuery] = useState("test");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -128,6 +128,7 @@ export default function App() {
         setError("");
         return;
       }
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -303,18 +304,21 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onCloseMovie();
   }
 
-  useEffect(function () {
-    document.addEventListener("keydown", function (e) {
-      if (e.code === "Escape") {
-        onCloseMovie();
-        console.log("CLOSING");
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
       }
-    });
+      document.addEventListener("keydown", callback);
 
-    return function(){
-      document.removeEventListener('keydown',)
-    }
-  }, []);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
 
   useEffect(
     function () {
